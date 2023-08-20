5fc151e20e4fd195554b6faadcaeffb92c008903
JayCohen <jay@jayco.dev>
2023-08-19 20:28:35 -0700

    Point in time.  Game and Team class in progress.

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,6 +1,8 @@
 from prettytable import PrettyTable
-import itertools
+import itertools as it
 import datetime
+from dateutil.relativedelta import relativedelta
+from dateutil.parser import parse
 import calendar
 
 
@@ -11,10 +13,73 @@ class Game:
         self.gameday = gameday
         self.hometeam = None
         self.awayteam = None
+        self.week_no = 0
 
     def __str__(self):
         return f'{self.gameday} - {self.field}'
 
+    def assign_week(self, start_date):
+        """
+        Assigns the week number based on the start date.
+
+        Parameters:
+            start_date (str): The start date of the tournament
+
+        Returns:
+            None
+        """
+        gameday = parse(self.gameday)
+        start = parse(start_date)
+        week_no = relativedelta(gameday, start).weeks
+        self.week_no = week_no
+
+
+class Team:
+    def __init__(self, team_name, level=''):
+        self.team_name = team_name
+        self.level = level
+        self.game_count = self.home_game_count + self.away_game_count
+        self.home_game_count = 0
+        self.away_game_count = 0
+
+
+    def __str__(self):
+        if self.level == '':
+            return f'{self.team_name}'
+        else:
+            return f'{self.level} - {self.team_name}'
+
+
+def create_games(start_date, end_date, gamedays, fields, time_slots):
+    """
+    Return a list of Games based on passed parameters.
+    :type start_date: str
+    :type end_date: str
+    :type gamedays: list
+    :type fields: list
+    :type time_slots: list
+    :param start_date:
+    :param end_date:
+    :param gamedays:
+    :param fields:
+    :param time_slots:
+    :return: list of Game objects
+    """
+    if not isinstance(start_date, str):
+        raise TypeError('start_date must be a string.')
+    if not isinstance(end_date, str):
+        raise TypeError('end_date must be a string.')
+    if not isinstance(gamedays, list):
+        raise TypeError('gamedays must be a list.')
+    if not isinstance(fields, list):
+        raise TypeError('fields must be a list.')
+    if not isinstance(time_slots, list):
+        raise TypeError('time_slots must be a list.')
+    
+    dates = eligible_game_days(start_date, end_date, gamedays)
+    games = list(it.product(fields, dates, time_slots))
+    games_list = [Game(game[0],game[2], game[1]) for game in games]
+    return games_list
 def groups(teams):
     """
     Create 2 equal sized groups of teams from a single list of teams.
